6258d2cf05044b40e491f4472d4c37907cb3a83d
Redot2468 <lawalridwan860@gmail.com>
2025-03-20 15:24:22 +0100

    fix: Fix the the color of the nav icon on hover

--- app/_ui/NavIcon.tsx
+++ app/_ui/NavIcon.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { usePathname } from "next/navigation";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 interface NavIconProp {
@@ -29,17 +28,17 @@ export default function NavIcon({
   return (
     <div
       onClick={() => router.push(href)}
-      className={`nav-link flex flex-col pt-2 pb-3 gap-1  items-center  max-w-[104px] ${isLinkActive && "border-b-4 border-b-green lg:border-b-0 lg:border-l-4 lg:border-l-green bg-beige-100 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg"} w-full lg:flex-row lg:items-center lg:max-w-full border lg:px-8 lg:gap-4  lg:pt-4 py-4 cursor-pointer group overflow-hidden`}
+      className={`nav-link flex max-w-[104px] flex-col items-center gap-1 pt-2 pb-3 ${isLinkActive && "border-b-green lg:border-l-green bg-beige-100 rounded-t-lg border-b-4 lg:rounded-t-none lg:rounded-r-lg lg:border-b-0 lg:border-l-4"} group w-full cursor-pointer overflow-hidden border py-4 lg:max-w-full lg:flex-row lg:items-center lg:gap-4 lg:px-8 lg:pt-4`}
     >
       <div className="icon">
         <Icon
           fill={isLinkActive ? "#277c78" : "#b3b3b3"}
-          styles="group-hover:fill-grey-100 transition-colors"
+          styles={` transition-colors ${isLinkActive ? "group-hover:fill-[#064d4a]" : "group-hover:fill-grey-100"} `}
         />
       </div>
 
       <p
-        className={`text-preset-5-bold lg:text-base hidden sm:block  group-hover:text-grey-100 transition-colors capitalize ${isLinkActive ? "text-grey-900" : "text-grey-300"} ${isMinimizeMenu ? "lg:w-0" : "lg:w-full"}transition-all duration-1000 border`}
+        className={`text-preset-5-bold group-hover:text-grey-100 hidden capitalize transition-colors sm:block lg:text-base ${isLinkActive ? "text-grey-900" : "text-grey-300"} ${isMinimizeMenu ? "lg:w-0" : "lg:w-full"}transition-all border duration-1000`}
       >
         {text}
       </p>
