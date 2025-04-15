5da9b9fa1f6f400ab2a42ffe91fdc50f13fcb6e2
Redot2468 <lawalridwan860@gmail.com>
2025-04-15 16:51:04 +0100

    fix: Remove borders

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-grow flex-col gap-[32px] border lg:flex-row lg:items-center">
+    <div className="flex min-h-screen flex-grow flex-col gap-[32px] lg:flex-row lg:items-center">
       <AuthNavbar />
 
-      <div className="flex flex-1 items-center justify-center border px-6 pb-8 lg:px-10 lg:py-8 lg:pb-0">
+      <div className="flex flex-1 items-center justify-center px-6 pb-8 lg:px-10 lg:py-8 lg:pb-0">
         {children}
       </div>
     </div>
